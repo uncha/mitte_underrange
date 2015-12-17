027ffe27ebfe1dfac98ea1f19353a66c9ffc558d
uncha <kyutae21c@naver.com>
2015-12-18 00:11:30 +0900

    board list
    bootstrap table, paging make board list

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var mongoose = require('mongoose');
 var app = express();
 
+/*************************** DB Connect *******************************/
 mongoose.connect('mongodb://uncha:rbxo6727@ds033175.mongolab.com:33175/mitte_underrange');
 var db = mongoose.connection;
 db.once('open', function(){
@@ -12,17 +13,27 @@ db.on('error', function(err){
 	console.log('DB Error :', err);
 });
 
+
+/*************************** express *******************************/
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname + '/public')));
 
+
+/*************************** router *******************************/
 app.get('/', function(req, res){
 	res.render('index');
 });
 
-app.get('/company/company01', function(req, res){
-	res.render('company/company01');
+app.get('/company/introduce', function(req, res){
+	res.render('company/introduce');
 });
 
+app.get('/customer/notice', function(req, res){
+	res.render('board/default/list');
+});
+
+
+/*************************** listen *******************************/
 app.listen(3000, function(){
 	console.log('Server on~!');
 });

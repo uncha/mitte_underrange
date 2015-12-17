514a275be2a60345c0ab10ebfdb599576e3e357a
uncha <kyutae21c@naver.com>
2015-12-17 22:13:24 +0900

    router setting
    router setting

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('init');
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.get('/company/company01', function(req, res){
+	res.render('company/company01');
+});
+
+app.listen(3000, function(){
+	console.log('Server on~!');
+});

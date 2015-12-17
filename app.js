a0cb13c2ae019599eb5d946755c872eebb3851d8
uncha <kyutae21c@naver.com>
2015-12-17 22:42:56 +0900

    DB Connected~!
    DB Connected~!
    require mongodb
    require mongoose

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var path = require('path');
-
+var mongoose = require('mongoose');
 var app = express();
+
+mongoose.connect('mongodb://uncha:rbxo6727@ds033175.mongolab.com:33175/mitte_underrange');
+var db = mongoose.connection;
+db.once('open', function(){
+	console.log('DB Connected');
+});
+db.on('error', function(err){
+	console.log('DB Error :', err);
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname + '/public')));
